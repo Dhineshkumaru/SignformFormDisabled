db7ad2c213f199e971fda3a520a58ed55356c74a
dhinesh kumar <38152518+dineshkumar46146@users.noreply.github.com>
2018-08-10 00:46:53 +0530

    Login form with border validation

--- scripts/common.js
+++ scripts/common.js
@@ -1,12 +1,8 @@
-var signinBtn = document.getElementById('signinBtn');
-signinBtn.disabled = true;
-
-
-function validateForm() {
-	var form  = document.forms['signinForm'];
-	if(form.checkValidity()) {
-		signinBtn.disabled = false;
+function fieldValidation(field) {
+	var formField = document.forms["signupForm"][field];
+	if(!formField.checkValidity()) {
+		document.getElementById(field).className = "borderColor";
 	} else {
-		signinBtn.disabled = true;
+		document.getElementById(field).className = "";
 	}
 }
